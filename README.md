bfcf020ac17333fe6bb1cb41ab39d9d4864fa6b5
Potader <igarc2001@gmail.com>
2026-03-22 19:28:55 -0500

    added documentation

---
+++ README.md
@@ -0,0 +1,7 @@
+# CSC 350 Week 2 Assignment - JSON and Bootstrap
+
+This assignment is to practice JSON object useage and an introduction to Bootstrap 5.
+
+## PS
+
+I could not get O-O to work with JSON parse, so this code is not in an object, unfortunately. This whole assignment through me for an extra loop, I thought I had a better grasp on looping through data than I did.
--- js/main.js
+++ js/main.js
@@ -1,8 +1,12 @@
+// JSON data for favourite singers
 let favouriteSingers =
   '[{"name":"Jody Gadsden","band":"Autoheart","DOB":"Unknown","bestSongs":"The Witching Hour, We Can Build a Fire"},{"name":"Jonathan Morali","band":"Syd Matters","DOB":"5/22/1980","bestSongs":"Obstacles, River Sister"},{"name":"Jerome Alexander","band":"Message to Bears","DOB":"1985","bestSongs":"Mountains, You Are a Memory"},{"name":"Emilee Petersmark/Kate Pillsbury (Duo vocals)","band":"The Crane Wives","DOB":"Founded: 2010","bestSongs":"Curses, How to Rest"}]';
+// Pulls in table as variable singerList, and parses JSON data into workable JavaScript object
 let singerList = document.getElementById("singer-list");
 const singers = JSON.parse(favouriteSingers);
 
+// This accepts each key:value pair in the JSON data, creates a new table row,
+// then puts the values from the pair into the new cells created
 singers.forEach((item) => {
   const newRow = document.createElement("tr");
 
